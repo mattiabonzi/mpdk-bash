2ea9191870a282b6217884cd8bf420b4c2c84a7f
Mattia Bonzi <mattia@mattiabonzi.it>
2023-01-24 00:06:17 +0100

    pre change iroot

--- .asset/test/new.sh
+++ .asset/test/new.sh
@@ -39,7 +39,7 @@ fi
 ######### BEGIN #########
 RUNS $mpdk new mpdktest1 #Dev instance
 OK -d ./mpdktest1/moodle
-OK -f ./mpdktest1/moodle/.mpdkinstance 
+OK -f ./mpdktest1/mpdkinstance.env 
 OK -f ./mpdktest1/moodle/version.php 
 OK -f ./mpdktest1//moodle/admin/tool/pluginskel/version.php 
 OK -f ./mpdktest1/moodle/local/codechecker/version.php 
@@ -55,7 +55,7 @@ cd "$root"
 RUNS $mpdk new -t mpdktest2 #Test only instance
 GREP "Instance ready, located in"
 OK -d ./mpdktest2/moodle #Moodle is present
-OK -f ./mpdktest2/moodle/.mpdkinstance #Check taht is an instance
+OK -f ./mpdktest2/mpdkinstance.env #Check taht is an instance
 OK -f ./mpdktest2/moodle/version.php #Moodle is actually moodle
 RUNS ls $asset/cache #Check cache folder isn't empty (the cache has been used)
 GREP . #Check cache folder isn't empty (the cache has been used)
@@ -66,7 +66,7 @@ OK -f ./mpdktest2/moodle/mpdkcached #Check that the cache has been used
 
 RUNS $mpdk new -v 4.1.0 mpdktest3 #Specific version
 OK -d ./mpdktest3/moodle #Moodle exist
-OK -f ./mpdktest3/moodle/.mpdkinstance #Is an instance
+OK -f ./mpdktest3/mpdkinstance.env #Is an instance
 OK -f ./mpdktest3/moodle/version.php #Moodle exist
 RUNS cat ./mpdktest3/moodle/version.php #Check it's the right version
 GREP 20221128 #Check it's the right version
@@ -75,7 +75,7 @@ IS $(ls .asset/cache | wc -l) -eq 2 #Check that now there are 2 version cached
 RUNS $mpdk new -c mpdktest4 #Without the cache
 OK -d ./mpdktest4/moodle #Moodle exist
 OK -f ./mpdktest4/moodle/version.php #Moodle exist
-OK -f ./mpdktest4/moodle/.mpdkinstance #Is an instance
+OK -f ./mpdktest4/mpdkinstance.env #Is an instance
 NOK -f ./mpdktest4/moodle/mpdkcached #Check that the cache hasn't been used
 ######### END #########
 
